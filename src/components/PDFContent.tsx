0015a9e83206074f322b26e80702f9d4568dc535
cdomizi <domizicesare@gmail.com>
2025-02-18 19:40:17 +0100

    remove unnecessary text, minor refactor

--- src/components/PDFContent.tsx
+++ src/components/PDFContent.tsx
@@ -1,8 +1,4 @@
-import {
-  getQuizSubtitle,
-  getQuizTitle,
-  splitText,
-} from "@components/PDFService";
+import { getQuestions, getQuizTitle } from "@components/PDFService";
 import { useEffect, useState } from "react";
 import { pdfjs } from "react-pdf";
 import { FileSelect } from "./FileSelect";
@@ -16,15 +12,16 @@ export function PDFContent() {
   const [filePath, setFilePath] = useState<string>("./Domande_2575_v81.pdf");
 
   const [quizTitle, setQuizTitle] = useState<string>();
-  const [quizSubtitle, setQuizSubtitle] = useState<string>();
-  const [quizContent, setQuizContent] = useState<string[]>();
+  const [questions, setQuestions] = useState<string[]>();
 
   useEffect(() => {
     const getTextFromPDF = async () => {
       try {
+        let quizContent = "";
+
         const pdf = await pdfjs.getDocument(filePath).promise; // Load PDF document
         const totalPageCount = pdf.numPages;
-        let text = "";
+
         // Loop through each page and extract the text
         for (
           let currentPage = 1;
@@ -32,20 +29,20 @@ export function PDFContent() {
           currentPage++
         ) {
           const page = await pdf.getPage(currentPage);
-          const pageText = await page.getTextContent();
-          text += pageText.items
+          const pageContent = await page.getTextContent();
+          quizContent += pageContent.items
             .map((item) => "str" in item && item.str)
             .join(" ");
         }
 
-        setQuizTitle(getQuizTitle(text));
-        setQuizSubtitle(getQuizSubtitle(text));
+        setQuizTitle(getQuizTitle(quizContent)); // Set quiz title
 
-        const textSplit = splitText(text);
+        // Remove question titles
+        const questionsContent = getQuestions(quizContent);
+
+        setQuestions(questionsContent); // Set questions
 
         await pdf.cleanup(); // Close PDF document
-
-        setQuizContent(textSplit); // Update state
       } catch (error) {
         console.error("Error extracting PDF text:", error);
       }
@@ -61,9 +58,12 @@ export function PDFContent() {
     <>
       <FileSelect onSelect={onFileSelect} />
       <h1>{quizTitle}</h1>
-      <h2>{quizSubtitle}</h2>
+      <p>
+        Concorso ordinario 2023 - Scuola secondaria di primo e secondo grado
+      </p>
+      <p>Classe di concorso 2575</p>
       <div>
-        {quizContent?.map((question, index) => <p key={index}>{question}</p>)}
+        {questions?.map((question, index) => <p key={index}>{question}</p>)}
       </div>
     </>
   );
