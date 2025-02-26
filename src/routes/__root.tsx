664cb320c0a316872605c86fdbeb513d16c11cd1
cdomizi <domizicesare@gmail.com>
2025-02-26 11:06:05 +0100

    remove router dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,11 @@
 import { TQuizContext } from "@/contexts/QuizContext";
 import { NotFound } from "@components/NotFound";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRouteWithContext<TQuizContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
   notFoundComponent: () => <NotFound data />,
